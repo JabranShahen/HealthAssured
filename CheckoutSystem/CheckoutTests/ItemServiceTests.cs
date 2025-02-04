1896e6179ce819987d1185e457f625d8e1ab7c21
Jabran Shaheen <34131257+JabranShahen@users.noreply.github.com>
2025-02-04 22:49:49 +0000

    Added failing unit test AddItem_ItemAddedSuccessfully for ItemService class to validate item addition functionality.

---
+++ CheckoutSystem/CheckoutTests/ItemServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Implementations.Services;
+using CheckoutSystem.Abstractions.Entities;
+
+namespace UnitTests.Services
+{
+    [TestClass]
+    public class ItemServiceTests
+    {
+        [TestMethod]
+        public void AddItem_ItemAddedSuccessfully()
+        {
+            // Arrange
+            var itemService = new ItemService();
+            var item = new Item { SKU = "A", UnitPrice = 50 };
+
+            // Act
+            itemService.AddItem(item);
+
+            // Assert
+            var retrievedItem = itemService.GetItem("A");
+            Assert.AreEqual(item, retrievedItem);
+        }
+
+    }
+}
