7c15526ce6a32fa20cb415b07c0a6033a808706a
Jabran Shaheen <34131257+JabranShahen@users.noreply.github.com>
2025-02-04 22:56:26 +0000

    test: Add failing unit test for adding promotion

---
+++ CheckoutSystem/CheckoutTests/PromotionServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Implementations.Services; // Assuming PromotionService is in this namespace
+using CheckoutSystem.Abstractions.Entities;
+using System.Collections.Generic;
+
+namespace UnitTests.Services
+{
+    [TestClass]
+    public class PromotionServiceTests
+    {
+        [TestMethod]
+        public void AddPromotion_PromotionAddedSuccessfully()
+        {
+            // Arrange
+            var promotionService = new PromotionService();
+            var promotion = new Promotion { Name = "Promo1", AssociatedItemSKU = "A", Quantity = 3, Price = 130 };
+
+            // Act
+            promotionService.AddPromotion(promotion);
+
+            // Assert
+            var retrievedPromotion = promotionService.GetPromotionsForItem("A");
+            CollectionAssert.Contains((System.Collections.ICollection?)retrievedPromotion, promotion);
+        }
+       
+    }
+}
