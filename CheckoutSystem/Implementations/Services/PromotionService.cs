37a3e6e62c59d9f021d8738806988a7a24418f81
Jabran Shaheen <34131257+JabranShahen@users.noreply.github.com>
2025-02-04 22:57:49 +0000

    feat: Implemented PromotionService methods
    Implemented the AddPromotion and GetPromotionsForItem methods in the PromotionService class to fulfill the contract specified by the IPromotionService interface. Now, the unit test AddPromotion_PromotionAddedSuccessfully in PromotionServiceTests passes successfully.

--- CheckoutSystem/Implementations/Services/PromotionService.cs
+++ CheckoutSystem/Implementations/Services/PromotionService.cs
@@ -1,19 +1,28 @@
 ﻿using CheckoutSystem.Abstractions.Entities;
 using CheckoutSystem.Abstractions.Services;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Implementations.Services
 {
     public class PromotionService : IPromotionService
     {
+        private readonly List<Promotion> _promotions;
+
+        public PromotionService()
+        {
+            _promotions = new List<Promotion>();
+        }
+
         public void AddPromotion(Promotion promotion)
         {
-            throw new System.NotImplementedException();
+            _promotions.Add(promotion);
         }
 
         public IEnumerable<Promotion> GetPromotionsForItem(string sku)
         {
-            throw new System.NotImplementedException();
+            return _promotions.Where(p => p.AssociatedItemSKU == sku);
         }
     }
 }
